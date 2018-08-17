642ef79c28e74e52c822795422bf1ec52931e070
Kirby Campbell <jkirbycampbell@gmail.com>
2018-08-17 15:22:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file
 function findMatching(collection, arg){
-  return collection.filter(function (item) { return item === arg; });
+  return collection.filter(function (item) { return item.toUpperCase() === arg.toUpperCase(); });
 }
