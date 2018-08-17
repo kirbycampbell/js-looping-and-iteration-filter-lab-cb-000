5cc623af3779b45ce41188aae2902379b1188d8f
Kirby Campbell <jkirbycampbell@gmail.com>
2018-08-17 15:24:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 // Code your solution in this file
 function findMatching(collection, arg){
-  return collection.filter(function (item) { return item.toUpperCase() === arg.toUpperCase(); });
+  return collection.filter(function (item) {
+    return item.toUpperCase() === arg.toUpperCase();
+  });
+}
+
+function fuzzyMatch(collection, arg){
+  return collection.filter(function (item) {
+    return item
+  });
 }
